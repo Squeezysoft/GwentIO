3b771ca8a1543616c8082e5fb7ff7bea461aaf25
Doo Doo Squeezy <squeezysoft@gmail.com>
2023-11-12 17:22:24 -0500

    Fiddling with file serving.

--- server.ts
+++ server.ts
@@ -1,12 +1,18 @@
 import express, { Request, Response } from 'express';
-import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log('url:', import.meta.url, __filename, __dirname);
+
+app.use(express.static(__dirname));
 
 app.get('*', (request: Request, response: Response) => {
-  response.sendFile(path.join(__dirname, 'dist/index.html'));
+  response.sendFile(new URL('index.html', __filename).pathname);
 });
 
 const port = process.env['PORT'] || 3000;
